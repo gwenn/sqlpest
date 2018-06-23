1d77d2ca2e3c69e48b324dd8988463c502b53c90
gwenn <gtreguier@gmail.com>
2018-06-23 11:12:32 +0200

    Upgrade to pest 1.0

--- src/test.rs
+++ src/test.rs
@@ -1,16 +1,17 @@
-use pest::prelude::*;
-use super::Rdp;
+use super::{Rule, SqlParser};
+use pest::Parser;
 
 macro_rules! assert_parse {
     ($s:expr, $c:ident) => {{
-        let mut parser = Rdp::new(StringInput::new($s));
-        assert!(parser.$c());
-        /*if !parser.$c() || !parser.end() {
-            println!("pos: {:?}", parser.input().pos());
-            println!("expected: {:?}", parser.expected());
-            println!("queue: {:?}", parser.queue());
-        }*/
-        assert!(parser.end());
+        let pairs = SqlParser::parse(Rule::$c, $s).unwrap_or_else(|e| panic!("{}", e));
+        println!("{:?} =>", $s);
+        for pair in pairs.flatten() {
+            let span = pair.clone().into_span();
+            // A pair is a combination of the rule which matched and a span of input
+            println!("Rule:    {:?}", pair.as_rule());
+            //println!("Span:    {:?}", span);
+            println!("Text:    {}", span.as_str());
+        }
     }};
 }
 
@@ -29,11 +30,15 @@ fn test_alter_table() {
 fn test_create_table() {
     assert_parse!("CREATE TABLE test (col)", create_table);
     assert_parse!("CREATE TABLE main.test (col)", create_table);
-    assert_parse!("CREATE TABLE test (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id INTEGER PRIMARY KEY NOT NULL, name TEXT NOT NULL)",
+        create_table
+    );
 
-    assert_parse!("CREATE TABLE test (id INTERGER NOT NULL, PRIMARY KEY (id))",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id INTERGER NOT NULL, PRIMARY KEY (id))",
+        create_table
+    );
     assert_parse!("CREATE TABLE test AS SELECT 1", create_table);
 
     assert_parse!("CREATE TEMP TABLE test (col)", create_table);
@@ -50,20 +55,28 @@ fn test_column_definition() {
     assert_parse!("CREATE TABLE test (id UNSIGNED BIG INT)", create_table);
     assert_parse!("CREATE TABLE test (id INT8)", create_table);
     assert_parse!("CREATE TABLE test (id CHARACTER(20))", create_table);
-    assert_parse!("CREATE TABLE test (id VARYING CHARACTER(255))",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id VARYING CHARACTER(255))",
+        create_table
+    );
     assert_parse!("CREATE TABLE test (id DOUBLE PRECISION)", create_table);
     assert_parse!("CREATE TABLE test (id DECIMAL(10,5))", create_table);
 }
 
 #[test]
 fn test_column_constraints() {
-    assert_parse!("CREATE TABLE test (id CONSTRAINT not_null NOT NULL)",
-                  create_table);
-    assert_parse!("CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT)",
-                  create_table);
-    assert_parse!("CREATE TABLE test (id INTEGER PRIMARY KEY ON CONFLICT IGNORE)",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id CONSTRAINT not_null NOT NULL)",
+        create_table
+    );
+    assert_parse!(
+        "CREATE TABLE test (id INTEGER PRIMARY KEY AUTOINCREMENT)",
+        create_table
+    );
+    assert_parse!(
+        "CREATE TABLE test (id INTEGER PRIMARY KEY ON CONFLICT IGNORE)",
+        create_table
+    );
     assert_parse!("CREATE TABLE test (id UNIQUE)", create_table);
     //assert_parse!("CREATE TABLE test (id CHECK (id > 0))", create_table);
     assert_parse!("CREATE TABLE test (id DEFAULT '')", create_table);
@@ -75,17 +88,25 @@ fn test_column_constraints() {
 
 #[test]
 fn test_table_constraints() {
-    assert_parse!("CREATE TABLE test (id, CONSTRAINT pk PRIMARY KEY (id))",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id, CONSTRAINT pk PRIMARY KEY (id))",
+        create_table
+    );
     assert_parse!("CREATE TABLE test (id, UNIQUE (id))", create_table);
     //assert_parse!("CREATE TABLE test (id, CHECK (id > 0))", create_table);
-    assert_parse!("CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable(id))",
-                  create_table);
-    assert_parse!("CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable)",
-                  create_table);
-    assert_parse!("CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable(id) DEFERRABLE \
-                   INITIALLY DEFERRED)",
-                  create_table);
+    assert_parse!(
+        "CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable(id))",
+        create_table
+    );
+    assert_parse!(
+        "CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable)",
+        create_table
+    );
+    assert_parse!(
+        "CREATE TABLE test (id, FOREIGN KEY (id) REFERENCES fktable(id) DEFERRABLE \
+         INITIALLY DEFERRED)",
+        create_table
+    );
 }
 
 #[test]
